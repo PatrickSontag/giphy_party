ba3edf60b6354609f4e0a0d9cb799dfbfb7242c5
Patrick Sontag <pmsontag84@gmail.com>
2022-03-31 15:44:47 -0600

    looks good, but CORB issues

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
 console.log("Let's get this party started!");
 
 async function getData(search) {
-    const response = await axios.get('http://api.giphy.com/v1/gifs/search', { params: {api_key: 'NXUw3cq8ctWgrRcocDkpTnCBbxEGXmBR', q: search}});
+    const response = await axios.get('http://api.giphy.com/v1/gifs/search', { params: 
+    {api_key: 'NXUw3cq8ctWgrRcocDkpTnCBbxEGXmBR',
+     q: search}});
 
-    console.log(response);
+    const gifList = response.data.data;
+    // const gif = gifList[0];
+    // console.log(gif);
+
+    const listLen = gifList.length;
+    console.log(listLen);
+
+    const gifNum = getRandom(listLen);
+    console.log(gifNum);
+
+    let count = 0;
+    
+    for (let gif of gifList) {
+        console.log(count)
+        if (count === gifNum) {
+            console.log("Loop, gif: ", gif);
+            console.log("Loop, gif.url: ", gif.url);
+            count = 0;
+            showGif(gif.url);
+            break;
+        }
+        count = count + 1;
+    }
+}
+
+const getRandom = (qty) => {
+    return Math.floor(Math.random() * qty);
+}
+
+function showGif(resGif) {
+    const gifLoc = document.getElementById('gif-spot');
+    const imgElement = document.createElement('IMG');
+    imgElement.src = `${resGif}`;
+    // const imgELement = document.createElement('IMG');
+    console.log("showGif, imgElement: ", imgElement);
+    console.log("showGif, resGif: ", resGif);
+    gifLoc.appendChild(imgElement);
 }
